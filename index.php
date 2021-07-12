1b077634abfb2b561859b2b00f7afb1bea1aebe4
mitsuhiro <papu.m0917@icloud.com>
2021-07-12 22:08:44 +0900

    ユーザーテーブルにインサートした情報を取得した

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// $link = mysqli_connect('localhost', 'samplephp', 'samplemysql', 'todolist');
+
+// 接続状況をチェックします
+// if (mysqli_connect_errno()) {
+//     die("データベースに接続できません:" . mysqli_connect_error() . "\n");
+// } else {
+//     echo "データベースの接続に成功しました。\n";
+// }
+
+try {
+    $db = new PDO('mysql:dbname=todolist;host=localhost;charset=utf8', 'samplephp', 'samplemysql');
+    echo "接続OK！";
+} catch (PDOException $e) {
+    echo 'DB接続エラー！: ' . $e->getMessage();
+}
