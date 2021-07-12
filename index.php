5f7e5fc3b4dff09338860ad9e7e168a3cd78dcf5
mitsuhiro <papu.m0917@icloud.com>
2021-07-12 23:33:54 +0900

    ヘッダー作成

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>タスク管理</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<div id="wrapper">
+    <div id="header">
+        <div class="header-list">
+            <ul>
+                <li><a href="">ログアウト</a></li>
+                <li><a href="">カテゴリー覧</a></li>
+                <li><a href="">＋</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="content">
+        <div class="content">
+            <div class="uncomplete-button">
+                <button type="submit" class="btn btn-primary">未完了</button>
+                <button type="submit" class="btn btn-primary">完了</button>
+            </div>
+        </div>
+    </div>
+</div>
