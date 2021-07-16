3c90ccd858c2c2187f07ad1f73f9d6dfd88c7e28
mitsuhiro <papu.m0917@icloud.com>
2021-07-16 23:26:32 +0900

    フォーム画面からユーザー情報を登録できるようにした

--- insert.php
+++ insert.php
@@ -2,9 +2,9 @@
 
 date_default_timezone_set('Asia/Tokyo');
 // (1) 登録するデータを用意
-$name = '北野';
-$email = 'kitano@gmail.com';
-$password = '12345678';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
 // (2) データベースに接続
 $pdo  = new PDO('mysql:charset=UTF8;dbname=todolist;host=localhost', 'samplephp', 'samplemysql');
@@ -23,9 +23,6 @@ $stmt->bindParam(':password', $password, PDO::PARAM_STR);
 
 // (5) SQL実行
 $res = $stmt->execute();
-var_dump($res);
-die;
-
 
 // (6) データベースの接続解除
 $pdo = null;
---
+++ signup.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>アドレス登録</title>
+</head>
+
+<body>
+    <p>会員登録</p>
+    <form action="insert.php" method="post">
+        <input type="text" name="name" size="10" placeholder="username" /><br />
+        <input type="text" name="email" size="30" placeholder="email" /><br />
+        <input type="text" name="password" size="30" placeholder="password"><br />
+        <!-- <input type="text" name="password" size="30" placeholder="password確認"><br /> -->
+        <input type="submit" value="登録する" />
+    </form>
+</body>
+
+</html>
