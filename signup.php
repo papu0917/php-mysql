75197236744345b8b60ab10d2415a778ced7a24d
mitsuhiro <papu.m0917@icloud.com>
2021-07-19 22:21:13 +0900

    ユーザーの新規登録をできるようにした

--- signup.php
+++ signup.php
@@ -1,19 +1,26 @@
+<!DOCTYPE html>
 <html>
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>アドレス登録</title>
+    <meta charset="utf-8">
+    <title>タスク管理</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <p>会員登録</p>
-    <form action="signupComplete.php" method="post">
-        <input type="text" name="name" size="10" placeholder="username" /><br />
-        <input type="text" name="email" size="30" placeholder="email" /><br />
-        <input type="text" name="password" size="30" placeholder="password"><br />
-        <input type="text" name="passwordConfirm" size="30" placeholder="password確認"><br />
-        <input type="submit" value="登録する" />
-    </form>
+    <div id="signup">
+        <div class="index">
+            <p>会員登録</p>
+            <form action="signupComplete.php" method="post">
+                <input class="width" type="text" name="name" size="10" placeholder="username" /><br />
+                <input class="width" ype="text" name="email" size="30" placeholder="email" /><br />
+                <input class="width" type="text" name="password" size="30" placeholder="password"><br />
+                <input class="width" type="text" name="passwordConfirm" size="30" placeholder="password確認"><br />
+                <input class="input" type="submit" value="アカウント作成" />
+            </form>
+            <p>topへ戻る</p>
+        </div>
+    </div>
 </body>
 
 </html>
