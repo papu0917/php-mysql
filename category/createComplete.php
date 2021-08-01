c5686ed668e603ec7f9ba3fcdc405a2b2070dc78
mitsuhiro <papu.m0917@icloud.com>
2021-08-01 22:28:40 +0900

    CategoryDao.phpの作成

--- category/createComplete.php
+++ category/createComplete.php
@@ -1,15 +1,18 @@
 <?php
 
-session_start();
+// session_start();
+require_once(__DIR__ . '/../Dao/CategoryDao.php');
+// require_once(dirname(__FILE__) . '/../Dao/CategoryDao.php');
 date_default_timezone_set('Asia/Tokyo');
-$userId = $_SESSION['id'];
+// $userId = $_SESSION['id'];
 $name = filter_input(INPUT_POST, 'name');
 
-$pdo  = new PDO('mysql:charset=UTF8;dbname=todolist;host=localhost', 'samplephp', 'samplemysql');
+$categoryDao = new CategoryDao();
+$name = $categoryDao->insert($name);
 
-$stmt = $pdo->prepare("INSERT INTO categories (name) VALUE (:name)");
-$stmt->bindValue(':name', $name, PDO::PARAM_STR);
-$res = $stmt->execute();
+// $stmt = $pdo->prepare("INSERT INTO categories (name) VALUE (:name)");
+// $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+// $res = $stmt->execute();
 $message = '登録できました。';
 
 $pdo = null;
