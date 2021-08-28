5279cf1c7aa6e26b9bdacb728a676624fff7d275
mitsuhiro <papu.m0917@icloud.com>
2021-08-28 22:20:37 +0900

    カテゴリーエンティティの作成

---
+++ Domain/Entity/Category.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/../../Infrastructure/Dao/CategoryDao.php';
+require_once __DIR__ . '/../../Domain/ValueObject/CategoryId.php';
+require_once __DIR__ . '/../../Domain/ValueObject/CategoryName.php';
+
+final class Category
+{
+    private $id;
+    private $name;
+
+    public function __construct(CategoryId $id, CategoryName $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function id(): CategoryId
+    {
+        return $this->id;
+    }
+
+    public function name(): CategoryName
+    {
+        return $this->name;
+    }
+}
