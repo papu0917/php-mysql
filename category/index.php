6181cb787a79259f25dc118b8a16e6f3f8068eb0
mitsuhiro <papu.m0917@icloud.com>
2021-09-29 20:57:36 +0900

    addEventListenerの作成

--- category/index.php
+++ category/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once(__DIR__ . '/../Session.php');
 Session::getInstance();
+
 require('getCategories.php');
 require('../getTask.php');
 ?>
@@ -13,38 +14,47 @@ require('../getTask.php');
     <link rel="stylesheet" href="../css/style.css">
 </head>
 
-<div id="wrapper">
-    <?php require('../header.php'); ?>
-    <div id="category">
-        <div class="content">
-            <h2>カテゴリ一覧</h2>
-            <div class="index">
-                <form action="createComplete.php" method="post">
-                    <input class="width" type="text" name="name" placeholder="カテゴリー追加" />
-                    <input class="input" type="submit" value="登録" />
-                </form>
+
+<body>
+    <div id="wrapper">
+        <?php require('../header.php'); ?>
+        <div id="category">
+            <div class="content">
+                <h2>カテゴリ一覧</h2>
+                <div class="index">
+                    <form action="createComplete.php" method="post">
+                        <input class="width" type="text" name="name" placeholder="カテゴリー追加" />
+                        <button id="submit">登録</button>
+                    </form>
+                </div>
+                <div class="table">
+                    <table class="table-list">
+                        <?php foreach ($categories as $category) : ?>
+                            <tr class="category">
+                                <td><?php echo $category['name']; ?></td>
+                                <td><a class="botann1" href="edit.php?id=<?php echo $category['id']; ?>">編集</a></td>
+                                <td>
+                                    <form action="delete.php" method="post">
+                                        <input type="submit" class="botann2" name="delete" value="削除" />
+                                        <input type="hidden" name="id" value="<?php echo $category['id']; ?>">
+                                    </form>
+                                </td>
+                                <!-- <td><a class="botann2" href="delete.php?id=<?php echo $category['id']; ?>">削除</a></td> -->
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
             </div>
-            <div class="table">
-                <table class="table-list">
-                    <?php foreach ($categories as $category) : ?>
-                        <tr class="category">
-                            <td><?php echo $category['name']; ?></td>
-                            <td><a class="botann1" href="edit.php?id=<?php echo $category['id']; ?>">編集</a></td>
-                            <td>
-                                <form action="delete.php" method="post">
-                                    <input type="submit" class="botann2" name="delete" value="削除" />
-                                    <input type="hidden" name="id" value="<?php echo $category['id']; ?>">
-                                </form>
-                            </td>
-                            <!-- <td><a class="botann2" href="delete.php?id=<?php echo $category['id']; ?>">削除</a></td> -->
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
-            </div>
+            <a class="botann" href="../index.php">戻る</a>
         </div>
-        <a class="botann" href="../index.php">戻る</a>
+        <?php require('../footer.php'); ?>
     </div>
-    <?php require('../footer.php'); ?>
-</div>
+    <script>
+        var btn = document.getElementById('submit');
+        btn.addEventListener('click', function() {
+            console.log('クリックされました！');
+        }, false);
+    </script>
+</body>
 
 </html>
